8333a0b18a41c9cb8e324018c38366981849d41e
MILES\jgeorge <jgeorge@striven.com>
2021-03-08 23:44:37 -0500

    html update

--- calculator.js
+++ calculator.js
@@ -1,11 +1,14 @@
 function valueButton(e){
-    calculator.screen.value +=e.value;
+    calculatorForm.screen.value +=e.value;
 }
 function clear(){
-    calculator.screen.value = null;
+    calculatorForm.screen.value = null;
 }
 
 function calculate(){
-    calculator.screen.value= eval(calculator.screen.value);
-    return document.getElementByName("calculator.screen.value");
+    calculatorForm.screen.value= eval(calculatorForm.screen.value);
+    console.log(calculatorForm.screen.value);
 }
+
+
+
